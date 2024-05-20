5f1991b7d2770f0761b9149ca0b17b46220baf4a
ItzRyz <zetsura.official04@gmail.com>
2024-05-20 13:24:49 +0700

    feat(lib): Added read .env support library

---
+++ src/lib/goDotEnv.go
@@ -0,0 +1,16 @@
+package lib
+
+import (
+	"log"
+	"os"
+
+	"github.com/joho/godotenv"
+)
+
+func GetEnvVar(key string) string {
+	err := godotenv.Load(".env")
+	if err != nil {
+		log.Fatalf("Error loading .env file")
+	}
+	return os.Getenv(key)
+}
